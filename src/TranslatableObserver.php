ddddd536ed2f81a3bac08f9d7e7fd5b661b29229
Rasmus Nielsen <rasmus@easify.dk>
2020-05-06 15:30:30 +0200

    Ensure syncing of siblings without global scopes (prevent falling out of sync)

--- src/TranslatableObserver.php
+++ src/TranslatableObserver.php
@@ -45,7 +45,7 @@ class TranslatableObserver
     public function saved(Model $model)
     {
         if ($this->shouldSyncSiblings($model)) {
-            $model->siblings->each->syncAttributesFromSibling($model);
+            $model->siblings()->get()->each->syncAttributesFromSibling($model);
         }
     }
 
