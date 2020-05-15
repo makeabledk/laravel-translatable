c03cb5267fa22c2e878c40340645e7f910207ccc
Rasmus Nielsen <rasmus@easify.dk>
2020-05-15 11:46:12 +0200

    Cleanup translatable relationsships trait

--- src/TranslatableRelationships.php
+++ src/TranslatableRelationships.php
@@ -4,11 +4,7 @@ namespace Makeable\LaravelTranslatable;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\MorphMany;
-use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Makeable\LaravelTranslatable\Builder\EloquentBuilder;
 use Makeable\LaravelTranslatable\Relations\TranslatedBelongsTo;
 use Makeable\LaravelTranslatable\Relations\TranslatedBelongsToMany;
 use Makeable\LaravelTranslatable\Relations\TranslatedHasMany;
@@ -18,11 +14,6 @@ use Makeable\LaravelTranslatable\Relations\TranslatedMorphTo;
 trait TranslatableRelationships
 {
     /**
-     * @var bool
-     */
-    protected $nextRelationWithoutTranslations = false;
-
-    /**
      * Create a new Eloquent query builder for the model.
      *
      * @param  \Illuminate\Database\Query\Builder  $query
@@ -30,17 +21,7 @@ trait TranslatableRelationships
      */
     public function newEloquentBuilder($query)
     {
-        return new \Makeable\LaravelTranslatable\Builder\EloquentBuilder($query);
-    }
-
-    /**
-     * @return $this
-     */
-    public function nonTranslatable()
-    {
-        $this->nextRelationWithoutTranslations = true;
-
-        return $this;
+        return new EloquentBuilder($query);
     }
 
     /**
@@ -55,12 +36,7 @@ trait TranslatableRelationships
      */
     protected function newBelongsTo(Builder $query, Model $child, $foreignKey, $ownerKey, $relation)
     {
-        return $this->appropriateRelation(
-            $query->getModel(),
-            BelongsTo::class,
-            TranslatedBelongsTo::class,
-            func_get_args()
-        );
+        return new TranslatedBelongsTo(...func_get_args());
     }
 
     /**
@@ -79,12 +55,7 @@ trait TranslatableRelationships
     protected function newBelongsToMany(Builder $query, Model $parent, $table, $foreignPivotKey, $relatedPivotKey,
                                         $parentKey, $relatedKey, $relationName = null)
     {
-        return $this->appropriateRelation(
-            $query->getModel(),
-            BelongsToMany::class,
-            TranslatedBelongsToMany::class,
-            func_get_args()
-        );
+        return new TranslatedBelongsToMany(...func_get_args());
     }
 
     /**
@@ -98,12 +69,7 @@ trait TranslatableRelationships
      */
     protected function newHasMany(Builder $query, Model $parent, $foreignKey, $localKey)
     {
-        return $this->appropriateRelation(
-            $query->getModel(),
-            HasMany::class,
-            TranslatedHasMany::class,
-            func_get_args()
-        );
+        return new TranslatedHasMany(...func_get_args());
     }
 
     /**
@@ -118,14 +84,7 @@ trait TranslatableRelationships
      */
     protected function newMorphMany(Builder $query, Model $parent, $type, $id, $localKey)
     {
-        return $this->appropriateRelation(
-            $query->getModel(),
-            MorphMany::class,
-            TranslatedMorphMany::class,
-            func_get_args()
-        );
-
-//        return new MorphMany($query, $parent, $type, $id, $localKey);
+        return new TranslatedMorphMany(...func_get_args());
     }
 
     /**
@@ -141,34 +100,6 @@ trait TranslatableRelationships
      */
     protected function newMorphTo(Builder $query, Model $parent, $foreignKey, $ownerKey, $type, $relation)
     {
-        // For morph-to query-model and parent model is the same, so we don't know
-        // at this point whether or not we'll be needing translatable.
-        return $this->translatableRelationshipAllowed()
-            ? new TranslatedMorphTo(...func_get_args())
-            : new MorphTo(...func_get_args());
-    }
-
-    /**
-     * @param  \Illuminate\Database\Eloquent\Model  $relatedModel
-     * @param  string  $originalRelation
-     * @param  string  $translatableRelation
-     * @param  array  $args
-     * @return mixed
-     */
-    protected function appropriateRelation(Model $relatedModel, $originalRelation, $translatableRelation, array $args)
-    {
-        return $this->translatableRelationshipAllowed() && (
-            ModelChecker::checkTranslatable($this) ||
-            ModelChecker::checkTranslatable($relatedModel)
-        )
-            ? new $translatableRelation(...$args)
-            : new $translatableRelation(...$args);
-    }
-
-    protected function translatableRelationshipAllowed()
-    {
-        return tap(! $this->nextRelationWithoutTranslations, function () {
-            $this->nextRelationWithoutTranslations = false; // Reset if was set
-        });
+        return new TranslatedMorphTo(...func_get_args());
     }
 }
